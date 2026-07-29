0f4d21ec65b0fd87172cc0a3c0f7b54a22062f5f
Vincenzo Russo <vincenzo.russo@platform.sh>
2026-07-29 16:39:54 +0200

    align page and article typography

--- pg/themes/pergrazia/functions.php
+++ pg/themes/pergrazia/functions.php
@@ -9,7 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'PERGRAZIA_VERSION', '1.7.0' );
+define( 'PERGRAZIA_VERSION', '1.7.1' );
 
 /**
  * Register theme features and navigation areas.
