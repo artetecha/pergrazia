8347459c45936c18e2e04293c924a0e371798efe
Vincenzo Russo <vincenzo.russo@platform.sh>
2026-08-04 13:38:00 +0200

    Handle wide featured artwork gracefully

--- pg/themes/pergrazia/functions.php
+++ pg/themes/pergrazia/functions.php
@@ -9,7 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'PERGRAZIA_VERSION', '1.7.3' );
+define( 'PERGRAZIA_VERSION', '1.7.5' );
 
 /**
  * Register theme features and navigation areas.
